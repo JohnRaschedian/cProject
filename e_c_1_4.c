3cd2d1f24114a770f77f48fdaa6e0744d8d78651
John Raschedian <jrashedian@gmail.com>
2016-11-19 12:11:49 +0100

    Create e_c_1_4.c

---
+++ e_c_1_4.c
@@ -0,0 +1,10 @@
+/* Demo: \', \", \\, \? */
+#include <stdio.h>
+void main() {
+	printf("\n\' \' \' \'");
+	printf("\n\" \" \" \"");
+	printf("\n\\ \\ \\ \\");
+	printf("\n\? \? \? \?");
+	printf("\n\"EKALAVYA\"");
+	printf("\n0 0 \0 \0 \0 \0");
+}
