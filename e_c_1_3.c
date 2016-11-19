514863c8bb6ff72d010bb202abdb39c06fca46b2
John Raschedian <jrashedian@gmail.com>
2016-11-19 11:31:25 +0100

    Create e_c_1_3.c

---
+++ e_c_1_3.c
@@ -0,0 +1,14 @@
+/* Demo: Using Newline Sequence in printf Statement */
+/* Writing starts in the Shape of Letter A */
+#include <stdio.h>
+void main() {
+	printf("                *					          \n");
+	printf("              *   *					        \n");
+	printf("            *       *				        \n");
+	printf("          *           *				      \n");
+	printf("        *               *			      \n");
+	printf("      *********************			    \n");
+	printf("    *                       *		    \n");
+	printf("  *                           *		  \n");
+	printf("*                               *	  \n");
+}
