4cb65156c9c8dc1eb0ccbe22e7f0930a031ebe4f
John Raschedian <jrashedian@gmail.com>
2016-11-20 07:00:19 +0100

    Create e_c_1_5.c

---
+++ e_c_1_5.c
@@ -0,0 +1,8 @@
+/* Demo: alert/beep Escape Sequence */
+#include <stdio.h>
+void main() {
+	printf("\a\t\t\t\t\t");
+	printf("\a\a\a\t\t\t");
+	printf("\a\a\a\a\a\t\t\t\t");
+	printf("\a\t\a\a\t\t\a\a\a\t\t\t\a\a");
+}
