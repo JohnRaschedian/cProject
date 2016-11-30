149f9ee9c4da81c9982daf8655d9606a00025078
John Raschedian <jrashedian@gmail.com>
2016-11-30 04:34:42 +0100

    Create ch1prg3c.c

---
+++ ch1prg3c.c
@@ -0,0 +1,14 @@
+// Author: John Raschedian - http://raschedian.com
+// Date: 11/30/2016
+// Program filename: ch1prg3c.c
+// Program Description: This program introduces some of the escape sequences
+//                      used in the c language such as \\ or \"
+
+#include <stdio.h>
+int main() {
+    printf("This is going to examine whether double quotes \" are going to be printed out or not.\n");
+    printf("This is going to examine whether single quotes \' is going to be printed out or not.\n");
+    printf("This is going to exmaine whether question mark \? is going be printed out or not.\n");
+    printf("This is going to examine whether backslash \\ is going to be printed out or not.\n");
+    return 0;
+}
