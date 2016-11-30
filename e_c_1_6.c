f65624208cf4bd635a9d516d2552404429e6e382
John Raschedian <jrashedian@gmail.com>
2016-11-30 05:16:56 +0100

    Delete e_c_1_6.c

--- e_c_1_6.c
+++
@@ -1,8 +0,0 @@
-/* Demo: alert/beep Escape Sequence */
-#include <stdio.h>
-void main() {
-	printf("\a\t\t\t\t\t");
-	printf("\a\a\a\t\t\t");
-	printf("\a\a\a\a\a\t\t\t\t");
-	printf("\a\t\a\a\t\t\a\a\a\t\t\t\a\a");
-}
