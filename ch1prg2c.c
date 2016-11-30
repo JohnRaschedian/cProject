f56e676590cd78673924010a75c22cce1311f70f
John Raschedian <jrashedian@gmail.com>
2016-11-30 03:02:33 +0100

    Create ch1prg2c.c

---
+++ ch1prg2c.c
@@ -0,0 +1,14 @@
+// Author: John Raschedian - http://raschedian.com
+// Date: 11/30/2016
+// Program filename: ch1prg2c.c
+// Program Description: This program shows how to use the new line sequence
+//                      to have the printf() function break its own line.
+
+#include <stdio.h>
+int main() {
+    printf("This is the first line.\n");
+    printf("This is the second line.\n");
+    printf("This is the third line.\n");
+    printf("This is the third line.\n");
+    return 0;
+}
