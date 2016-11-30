a82f31f0d32bcd42404225c1660d58303cb85c10
John Raschedian <jrashedian@gmail.com>
2016-11-30 05:14:57 +0100

    Create ch1prg5c.c

---
+++ ch1prg5c.c
@@ -0,0 +1,21 @@
+// Author: John Raschedian - http://raschedian.com
+// Date: 11/30/2016
+// Program filename: ch1prg5c.c
+// Program Description: This program demonstrates how the escape
+//                      sequence \t creates a shape like two parallel bars.
+
+
+#include <stdio.h>
+int main() {
+    printf("*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n*\t*\n");
+    return 0;
+}
+
+// This program will have the following output: 
+//    *       *
+//    *       *
+//    *       *
+//    *       *
+//    *       *
+//    *       *
+
