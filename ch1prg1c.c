350c3202b0750e34a4116fb08115ab4ba78e33b0
John Raschedian <jrashedian@gmail.com>
2016-11-30 02:19:48 +0100

    Create ch1prg1c.c

---
+++ ch1prg1c.c
@@ -0,0 +1,13 @@
+ /* Author: John Raschedian - http://raschedian.com
+ Date: 11/30/2016
+ Demo: printf() function - This program shows that
+ the printf() function does not automatically break its own line. */
+
+#include <stdio.h>
+int main() {
+    printf("This is the first line.");
+    printf("This is the second line.");
+    printf("This is the third line.");
+    printf("This is the forth line.");
+    return 0;
+}
