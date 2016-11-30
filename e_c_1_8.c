018e4649f57c487227272de34f3327274d579a1e
John Raschedian <jrashedian@gmail.com>
2016-11-30 05:17:08 +0100

    Delete e_c_1_8.c

--- e_c_1_8.c
+++
@@ -1,6 +0,0 @@
-/* Demo: horizontal tab */
-#include <stdio.h>
-void main() {
-	printf("12345678123456781234567812345678123456781234567812345678\n");
-	printf("A\tAB\tABC\tABCD\tABCDE\tABCDEFG\tABCDEFGH\n");
-}
