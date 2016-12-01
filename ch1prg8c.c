779103dddc3215350918d3eb49192ffa145b6a96
John Raschedian <jrashedian@gmail.com>
2016-12-01 08:48:38 +0100

    Create ch1prg8c.c

---
+++ ch1prg8c.c
@@ -0,0 +1,15 @@
+// Author: John Raschedian - http://raschedian.com
+// Date: 12/01/2016
+// Program filename: ch1prg8c.c
+// Program Description: In this program, you'll learn how the
+//                      the escape sequence \r works.
+
+
+#inclde <stdio.h>
+int main() {
+    printf("Hello \r there.");
+    return 0;
+}
+
+//  Output: 
+//  " there." (without the quotation marks)
