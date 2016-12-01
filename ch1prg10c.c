3b4009e2248465b0355f2a5d5a744a0335af7ba7
John Raschedian <jrashedian@gmail.com>
2016-12-01 11:56:57 +0100

    Create ch1prg10c.c

---
+++ ch1prg10c.c
@@ -0,0 +1,17 @@
+// Author: John Raschedian - http://raschedian.com
+// Date: 12/01/2016
+// Program filename: ch1prg10c.c
+// Program Description:
+
+#include <stdio.h>
+int main() {
+    int length = 5;
+    float width = 2.5;
+    printf("\nThe length of the rectangle is %d\nThe width of the ractangle is %f\n\n\n", length, width);
+    return 0;
+}
+
+
+//  Output: 
+//  The length of the rectangle is 5
+//  The width of the ractangle is 2.500000
