e776b2896c1f1e435f8b9297f06ad0d6061d31cf
John Raschedian <jrashedian@gmail.com>
2016-12-02 07:28:06 +0100

    Create ch1prg13c.c

---
+++ ch1prg13c.c
@@ -0,0 +1,21 @@
+// Author: John Raschedian - http://raschedian.com
+// Date: 12/02/2016
+// Program filename: ch1prg13c.c
+// Program Description: This program shows you how
+// you can calculations using the printf function.
+
+
+#include <stdio.h>
+int main() {
+    int a = 2;
+    float b = 1.2345;
+    printf("a\tb\t\ta * b\n");
+    printf("%d\t%f\t%f\n\n", a, b, a * b);
+    return 0;
+}
+
+
+
+//  Output:
+//  a   b           a * B
+//  2   1.234500    2.469000
